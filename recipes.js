7d8af22d83133e9210d44c3f3f38c83ca007f629
jenishxadhikari <114239379+jenishxadhikari@users.noreply.github.com>
2025-01-18 00:28:31 +0545

    refactor: improve code readability

--- recipes.js
+++ recipes.js
@@ -9,143 +9,8 @@ const form = document.querySelector("form")
 let selectedTags = []
 
 // Initial Display
-initialize()
-
-// Helper Functions
-async function initialize() {
-  await renderTags()
-  await renderAllRecipes()
-}
-
-async function renderTags() {
-  try {
-    displayTags.innerHTML += `<p class="text-xl">Loading...</p>`
-
-    const response = await fetch('https://dummyjson.com/recipes/tags')
-    const tags = await response.json()
-
-    if (tags.length > 0) {
-      displayTags.innerHTML = ""
-      {
-        tags.forEach(tag => {
-          displayTags.innerHTML += `
-          <li class="flex items-center gap-1">
-            <input type="checkbox" id="tag" value=${tag} class="size-4">
-            <span class="text-nowrap md:text-lg">${tag}</span>
-          </li>
-        `
-        });
-      }
-      document.querySelectorAll("#tag").forEach((tag) => {
-        tag.addEventListener("click", handleSelectedTags)
-      })
-    } else {
-      displayTags.innerHTML = ""
-      displayTags.innerHTML += "No tags found"
-    }
-  } catch (error) {
-    displayTags.innerHTML = "An error occurred while fetching tags.";
-    console.error('Error fetching tags:', error);
-  }
-}
-
-async function renderAllRecipes() {
-  displayRecipes.innerHTML += " "
-  displayRecipes.innerHTML += `<p class="text-3xl">Loading...</p>`
-  try {
-    const response = await fetch('https://dummyjson.com/recipes?limit=0&sortBy=name&order=asc')
-    const body = await response.json()
-
-    if (body && body.recipes) {
-      const recipes = body.recipes
-      displayRecipes.innerHTML = ""
-      {
-        recipes.forEach((recipe) => {
-          displayRecipes.innerHTML += `
-        <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
-          <img src=${recipe.image} alt=${recipe.name} class="">
-          <div class="h-full flex flex-col justify-between">
-            <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
-            <div class="p-2 flex justify-between items-center">
-              <p class="">⭐${recipe.rating}/5</p>
-              <p>${recipe.cookTimeMinutes + recipe.prepTimeMinutes} mins</p>
-            </div>
-          </div>
-        </div>
-      `
-        })
-      }
-    } else {
-      displayRecipes.innerHTML += ""
-      displayRecipes.innerHTML += "No Recipe Found"
-    }
-  } catch (error) {
-    displayRecipes.innerHTML = "An error occurred while fetching recipes.";
-    console.error('Error fetching recipes:', error);
-  }
-}
-
-async function renderRecipes() {
-  displayRecipes.innerHTML = ""
-  try {
-    if (selectedTags.length === 1) {
-      displayRecipes.innerHTML += `<p class="text-3xl">Loading...</p>`
-
-      const response = await fetch(`https://dummyjson.com/recipes/tag/${selectedTags[0]}`)
-      const body = await response.json()
-      renderFilteredRecipes(body.recipes)
-      selectedTags = []
-
-    } else if (selectedTags.length > 0) {
-      displayRecipes.innerHTML += `<p class="text-3xl">Loading...</p>`
-
-      let selectedRecipes = []
-      for (let i = 0; i < selectedTags.length; i++) {
-        const response = await fetch(`https://dummyjson.com/recipes/tag/${selectedTags[i]}`)
-        const body = await response.json()
-        const recipes = body.recipes
-        recipes.forEach(recipe => selectedRecipes.push(recipe))
-      }
-      // Remove repeated data
-      selectedRecipes = selectedRecipes.filter((item, index, self) =>
-        index === self.findIndex((t) => t.id === item.id)
-      )
-      renderFilteredRecipes(selectedRecipes)
-      selectedTags = []
-
-    } else {
-      renderAllRecipes()
-    }
-  } catch (error) {
-    displayRecipes.innerHTML = "An error occurred while fetching recipes.";
-    console.error('Error fetching recipes:', error);
-  }
-}
-
-function renderFilteredRecipes(recipes) {
-  if (recipes) {
-    displayRecipes.innerHTML = ""
-    {
-      recipes.forEach((recipe) => {
-        displayRecipes.innerHTML += `
-      <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
-        <img src=${recipe.image} alt=${recipe.name} class="">
-        <div class="h-full flex flex-col justify-between">
-          <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
-          <div class="p-2 flex justify-between items-center">
-            <p class="">⭐${recipe.rating}/5</p>
-            <p>${recipe.cookTimeMinutes + recipe.prepTimeMinutes} mins</p>
-          </div>
-        </div>
-      </div>
-    `
-      })
-    }
-  } else {
-    displayRecipes.innerHTML += ""
-    displayRecipes.innerHTML += "No Recipe Found"
-  }
-}
+renderTags()
+renderAllRecipes()
 
 // Event Handlers
 function handleOpenDialog() {
@@ -165,3 +30,114 @@ function handleSelectedTags(e) {
 // Event Listeners
 filter.addEventListener("click", handleOpenDialog)
 close.addEventListener("click", handleCloseDialog)
+
+// Helper Functions
+async function renderTags() {
+  try {
+    displayTags.innerHTML = `<p class="text-xl">Loading...</p>`
+
+    const response = await fetch('https://dummyjson.com/recipes/tags')
+    const tags = await response.json()
+
+    if (tags.length > 0) {
+      displayTags.innerHTML = ""
+      displayTags.innerHTML = tags.map(tag => `
+        <li class="flex items-center gap-1">
+          <input type="checkbox" id="tag" value="${tag}" class="size-4">
+          <span class="text-nowrap md:text-lg">${tag}</span>
+        </li>
+      `).join('');
+      document.querySelectorAll("#tag").forEach((tag) => {
+        tag.addEventListener("click", handleSelectedTags)
+      })
+    } else {
+      displayTags.innerHTML = ""
+      displayTags.innerHTML = "No tags found"
+    }
+  } catch (error) {
+    displayTags.innerHTML = "An error occurred while fetching tags.";
+    console.error('Error fetching tags:', error);
+  }
+}
+
+async function renderAllRecipes() {
+  displayRecipes.innerHTML = `<p class="text-3xl">Loading...</p>`
+  try {
+    const response = await fetch('https://dummyjson.com/recipes?limit=0&sortBy=name&order=asc')
+    const { recipes } = await response.json()
+
+    if (recipes.length > 0) {
+      displayRecipes.innerHTML = ""
+      displayRecipes.innerHTML = recipes.map(recipe =>
+        `
+        <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
+          <img src=${recipe.image} alt=${recipe.name} class="">
+          <div class="h-full flex flex-col justify-between">
+            <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
+            <div class="p-2 flex justify-between items-center">
+              <p class="">⭐${recipe.rating}/5</p>
+              <p>${recipe.cookTimeMinutes + recipe.prepTimeMinutes} mins</p>
+            </div>
+          </div>
+        </div>
+      `
+      ).join('')
+    } else {
+      displayRecipes.innerHTML = ""
+      displayRecipes.innerHTML = "No Recipe Found"
+    }
+  } catch (error) {
+    displayRecipes.innerHTML = "An error occurred while fetching recipes.";
+    console.error('Error fetching recipes:', error);
+  }
+}
+
+async function renderRecipes() {
+  displayRecipes.innerHTML = ""
+
+  if (selectedTags.length === 0) {
+    return await renderAllRecipes()
+  }
+
+  displayRecipes.innerHTML += `<p class="text-3xl">Loading...</p>`
+  try {
+    let selectedRecipes = []
+    for (let tag of selectedTags) {
+      const response = await fetch(`https://dummyjson.com/recipes/tag/${tag}`)
+      const { recipes } = await response.json()
+      selectedRecipes = [...selectedRecipes, ...recipes]
+    }
+    // Remove duplicated recipes
+    selectedRecipes = selectedRecipes.filter((item, index, self) =>
+      index === self.findIndex((t) => t.id === item.id)
+    )
+    renderFilteredRecipes(selectedRecipes)
+    selectedTags = []
+  }
+  catch (error) {
+    displayRecipes.innerHTML = "An error occurred while fetching recipes.";
+    console.error('Error fetching recipes:', error);
+  }
+}
+
+function renderFilteredRecipes(recipes) {
+  if (recipes.length > 0) {
+    displayRecipes.innerHTML = ""
+    displayRecipes.innerHTML = recipes.map(recipe => `
+      <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
+        <img src=${recipe.image} alt=${recipe.name} class="">
+        <div class="h-full flex flex-col justify-between">
+          <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
+          <div class="p-2 flex justify-between items-center">
+            <p class="">⭐${recipe.rating}/5</p>
+            <p>${recipe.cookTimeMinutes + recipe.prepTimeMinutes} mins</p>
+          </div>
+        </div>
+      </div>
+    `
+    ).join('')
+  } else {
+    displayRecipes.innerHTML = ""
+    displayRecipes.innerHTML = "No Recipe Found"
+  }
+}
--- script.js
+++ script.js
@@ -1,75 +1,49 @@
-const displayRecommendedRecipe = document.getElementById("displayRecommendedRecipe")
+const displayPopularRecipe = document.getElementById("displayPopularRecipe")
 const displayRecipeForBeginners = document.getElementById("displayRecipeForBeginners")
 
-async function renderRecommendedRecipe() {
-  try {
-    displayRecommendedRecipe.innerHTML += `<p class="text-3xl">Loading...</p>`
+renderRecipe(displayPopularRecipe, "popular")
+renderRecipe(displayRecipeForBeginners, "easy")
 
+async function renderRecipe(displayElement, type) {
+  displayElement.innerHTML += `<p class="text-3xl">Loading...</p>`
+
+  try {
     const response = await fetch('https://dummyjson.com/recipes?limit=0')
-    const body = await response.json()
-    
-    if (body && body.recipes) {
-      const recipes = body.recipes.filter((recipe) => recipe.rating > 4.8).slice(0, 8)
-      displayRecommendedRecipe.innerHTML = ""
-      {
-        recipes.forEach((recipe) => {
-          displayRecommendedRecipe.innerHTML += `
-        <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
-          <img src=${recipe.image} alt=${recipe.name} class="">
-          <div class="h-full flex flex-col justify-between">
-            <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
-            <div class="p-2 flex justify-between items-center">
-              <p class="">⭐${recipe.rating}/5</p>
-              <p>${recipe.cookTimeMinutes} mins</p>
+    const { recipes } = await response.json()
+
+    if (recipes) {
+      let filteredRecipes
+      if (type === "popular") {
+        // Filter recipes based on rating
+        filteredRecipes = recipes.filter((recipe) => recipe.rating > 4.8).slice(0, 8)
+      } else if (type === "easy") {
+        // Filter recipes based on difficulty
+        filteredRecipes = recipes.filter((recipe) => recipe.difficulty === "Easy").slice(0, 8)
+      }
+      // Render the recipes if available
+      if (filteredRecipes.length > 0) {
+        displayElement.innerHTML = filteredRecipes.map(recipe => `
+          <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
+            <img src=${recipe.image} alt=${recipe.name} class="">
+            <div class="h-full flex flex-col justify-between">
+              <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
+              <div class="p-2 flex justify-between items-center">
+                <p class="">⭐${recipe.rating}/5</p>
+                <p>${recipe.cookTimeMinutes + recipe.prepTimeMinutes} mins</p>
+              </div>
             </div>
           </div>
-        </div>
-      `
-        })
+        `).join('')
+      } else {
+        displayElement.innerHTML = ""
+        displayElement.innerHTML = "No Recipe Found"
       }
     } else {
-      displayRecommendedRecipe.innerHTML += "No Recipe Found"
+      displayElement.innerHTML = ""
+      displayElement.innerHTML = "No Recipe Found"
     }
   } catch (error) {
-    displayRecommendedRecipe.innerHTML = "An error occurred while fetching recipes.";
+    displayElement.innerHTML = "An error occurred while fetching recipes.";
     console.error('Error fetching recipes:', error);
   }
 }
-
-async function renderRecipeForBeginners() {
-  try {
-    displayRecipeForBeginners.innerHTML += `<p class="text-3xl">Loading...</p>`
-
-    const response = await fetch('https://dummyjson.com/recipes?limit=0')
-    const body = await response.json()
-    
-    if (body && body.recipes) {
-      const recipes = body.recipes.filter((recipe) => recipe.difficulty === "Easy").slice(0, 8)
-      displayRecipeForBeginners.innerHTML = ""
-      {
-        recipes.forEach((recipe) => {
-          displayRecipeForBeginners.innerHTML += `
-        <div class="flex flex-col border-2 border-zinc-500 rounded-md overflow-hidden">
-          <img src=${recipe.image} alt=${recipe.name} class="">
-          <div class="h-full flex flex-col justify-between">
-            <p class="font-semibold text-lg p-2 hover:underline decoration-emerald-500 underline-offset-2">${recipe.name}</p>
-            <div class="p-2 flex justify-between items-center">
-              <p class="">⭐${recipe.rating}/5</p>
-              <p>${recipe.cookTimeMinutes + recipe.prepTimeMinutes} mins</p>
-            </div>
-          </div>
-        </div>
-      `
-        })
-      }
-    } else {
-      displayRecipeForBeginners.innerHTML += "No Recipe Found"
-    }
-  } catch (error) {
-    displayRecipeForBeginners.innerHTML = "An error occurred while fetching recipes.";
-    console.error('Error fetching recipes:', error);
-  }
-}
-
-renderRecommendedRecipe()
-renderRecipeForBeginners()
